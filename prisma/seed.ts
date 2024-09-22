19b3e49484d69cf8cb09362d965d6e8cde4e46f7
Martin Lowe <martinlowe673@gmail.com>
2024-09-22 16:35:01 +0100

    more on seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,3 +4,18 @@ type Author ={
     firstName: string,
     lastName: string,
 }
+
+type Book ={
+    title: string,
+    isFiction:boolean,
+    datesPublished: Date;
+
+}
+
+function getAuthors() : Array<Author> {
+    return [
+        {firstName: "John", lastName: "Doe"},
+        {firstName: "Jane", lastName: "Doe"},
+        {firstName: "Bob", lastName: "Smith"},
+    ]
+}
